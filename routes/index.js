8813714727e0338505fce1824b0b8b0fa539e095
Maxim Zhosan <maxim.zhosan@gmail.com>
2023-01-23 23:40:54 +0100

    api

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const pageNotFoundError = (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 };
 
-index.post('/signup', celebrate({
+index.post('/api/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
@@ -18,17 +18,17 @@ index.post('/signup', celebrate({
   }).unknown(true),
 }), createUser);
 
-index.post('/signin', celebrate({
+index.post('/api/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
 
-index.post('/signout', logout);
+index.post('/api/signout', logout);
 
-index.use('/users', auth, require('./users'));
-index.use('/movies', auth, require('./movies'));
+index.use('/api/users', auth, require('./users'));
+index.use('/api/movies', auth, require('./movies'));
 
 index.use('/', auth, pageNotFoundError);
 

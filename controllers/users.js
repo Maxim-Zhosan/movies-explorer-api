5c3202f4af5d274e6c5982a796435ee1796a495c
Maxim Zhosan <maxim.zhosan@gmail.com>
2023-02-19 17:44:28 +0100

    bugfix: update userinfo3

--- controllers/users.js
+++ controllers/users.js
@@ -32,7 +32,7 @@ module.exports.updateProfile = (req, res, next) => {
   User.findOne({ email })
     .then((user) => {
       if (user && user._id !== req.user._id) {
-        next(new ConflictError(JSON.parse({user})));
+        next(new ConflictError({user: user, req:req.user }))
       } else {
         User.findByIdAndUpdate(
           req.user._id,

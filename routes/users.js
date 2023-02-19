6d1c9b596337a6cf607abd282a82a16c5b912e9f
Maxim Zhosan <maxim.zhosan@gmail.com>
2023-02-19 18:09:11 +0100

    bugfix: profile info savin2

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const {
 users.get('/me', getCurrentUser);
 users.patch('/me', celebrate({
   body: Joi.object().keys({
+    _id: Joi.string().required(),
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required(),
   }),

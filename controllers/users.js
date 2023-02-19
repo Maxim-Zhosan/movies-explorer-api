6f563d3acc70694e31b85f16ebf0f39437ac79c2
Maxim Zhosan <maxim.zhosan@gmail.com>
2023-02-19 18:25:52 +0100

    bugfix: profile info savin3

--- controllers/users.js
+++ controllers/users.js
@@ -31,7 +31,7 @@ module.exports.updateProfile = (req, res, next) => {
   User.findOne({ email })
     .then((user) => {
       if (user) {
-        next(new ConflictError(user._id === _id))
+        next(new ConflictError(user._id === req.body._id))
       } else {
         User.findByIdAndUpdate(
           req.user._id,
